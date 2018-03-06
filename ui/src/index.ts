b954901dee94cc5ca6320f0e01522e5f4b3e85b0
Simon Gordon <gordonsimon@hotmail.com>
2018-03-06 19:27:55 +1000

    Draw points on page

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,10 +1,30 @@
 const canvas = document.getElementById('gamecanvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
+interface Coords {
+  x: number;
+  y: number;
+}
+
+const points: Coords[] = [
+  {
+    x: 20,
+    y: 45
+  }
+];
+
 function draw() {
   ctx.fillStyle = '#000000';
   ctx.font = '30px Arial';
   ctx.fillText('Hello!', canvas.width / 2 - 20, canvas.height / 2);
+
+  for (let i = 0; i < points.length; i++) {
+    const { x, y } = points[i];
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+  }
 }
 
 setInterval(draw, 10);
